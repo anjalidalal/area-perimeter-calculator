e6a9766f1367e22a99a40721deec440dd5a5a5c1
anjalidalal <anjalimaria2002@gmail.com>
2023-08-04 15:02:03 +0530

    UI for drag and drop

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 let squares = document.querySelectorAll(".square");
 let grids = document.querySelectorAll(".grid");
-let resetButton = document.getElementById(reset);
+let resetButton = document.getElementById("reset");
 
 function dragStart(event) {
-  console.log("....dragging");
-
   event.dataTransfer.setData("text", event.target.id);
 }
 
 function dragOver(event) {
-  console.log("...drag over");
   event.preventDefault();
 }
 
 function drop(event) {
   event.preventDefault();
-  console.log("...drop");
 
   let data = event.dataTransfer.getData("text");
   let draggedElement = document.getElementById(data);
@@ -26,9 +22,8 @@ function drop(event) {
 }
 
 function handleReset() {
-  squares.forEach((square) => {
-    square.remove();
-  });
+  console.log("reset");
+  window.location.reload();
 }
 
 squares.forEach((square) => {
